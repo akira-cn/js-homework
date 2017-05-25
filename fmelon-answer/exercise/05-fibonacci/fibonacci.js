c0d9878e529867334df8e0488523ba337b747f8a
AlexWang <alexwangcss@gmail.com>
2017-05-25 08:32:16 +0800

    王凯平 homework

---
+++ fmelon-answer/exercise/05-fibonacci/fibonacci.js
@@ -0,0 +1,22 @@
+function fibonacci(n ){
+    if(n <= 2) alert(1);
+    else {
+      var a = 1;
+      var b = 1;
+      var t;
+      for (var i = 2; i < n; i++) {
+        t = a + b;
+        a = b;
+        b = t;
+      }
+    alert("求得的fibonacci数值为："+b);
+    }
+}
+
+var num = prompt("请输入一个大于等于1的数");
+if(parseInt(num) >= 1 ){
+    fibonacci(num);
+}
+else{
+    alert("输入的数非法");
+}
