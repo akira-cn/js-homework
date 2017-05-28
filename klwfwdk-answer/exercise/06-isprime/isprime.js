64cf089488b4a36e6ac420ac35fc744ac2a04283
klwfwdk <klwfwdk@qq.com>
2017-05-28 23:47:33 +0800

    1
    Signed-off-by: klwfwdk <klwfwdk@qq.com>

---
+++ klwfwdk-answer/exercise/06-isprime/isprime.js
@@ -0,0 +1,16 @@
+function isPrime(n) {
+
+    let a = parseInt(Math.sqrt(n));
+
+    for (var i = 2; i <= a; i++) {
+        if (n % i === 0) {
+            return false;
+        }
+
+    }
+    return true;
+}
+
+for (var i = 1; i < 10; i++) {
+    console.log(isPrime(i));
+}
