742a0e5bd5a5b32743ca58d9797649ada77aa844
gaominqi <471892205@qq.com>
2017-06-04 10:15:51 +0800

    update

---
+++ gaominqi-answer/08-reverse/08-reverse.js
@@ -0,0 +1,5 @@
+function reverse(str) 
+{
+      var newArray=str.split('');
+      return newArray.reverse().join('');
+}
--- gaominqi-answer/09-reverselist/switch.js
+++
@@ -1,8 +0,0 @@
-var input;
-input=parseInt(prompt());
-if(input%2==0)
-console.log("奇数");
-else if(input%2==1)
-console.log("偶数");
-else
-console.log("不是整数");
