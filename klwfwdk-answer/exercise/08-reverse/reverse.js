3ed535bd7922cf2e4ee2fb4bd805a2acb59fa77b
klwfwdk <klwfwdk@qq.com>
2017-06-04 15:37:31 +0800

    1

--- klwfwdk-answer/exercise/08-reverse/reverse.js
+++ klwfwdk-answer/exercise/08-reverse/reverse.js
@@ -3,7 +3,7 @@ function reverse(str) {
     for (var index = 1; index <= str.length; index++) {
         a += str[str.length - index];
     }
-    return a;
+    return a.split(' ').reverse().join(' ');
 }
 
 console.log(reverse("asdefg"));
